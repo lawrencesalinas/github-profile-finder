fddbcff287e547f0772e78727163dcdeba30117f
Lawrence <lawrencesalinas@yahoo.com>
2022-04-01 02:10:06 -0400

    created footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Footer from './components/layout/Footer';
 import NavBar from './components/layout/NavBar';
 
 function App() {
   return (
   <Router>
-    <div className='.flex.flex-col.justify-between.h-screen'>
+    <div className='flex flex-col justify-between h-screen'>
       <NavBar/>
-      <main>
+      <main className='container mx-auto px-3 pb-12'>
         content
       </main>
+      <Footer/>
     </div>
   </Router>
   );
