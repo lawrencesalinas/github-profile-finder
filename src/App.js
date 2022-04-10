e7c0cbb34c164e3baebc1aa5cd48428a6defa1f2
Lawrence <lawrencesalinas@yahoo.com>
2022-04-10 12:57:33 -0400

    added userReducer for userResults;

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import NavBar from './components/layout/NavBar';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
+import { GithubProvider } from './context/github/GithubContext';
 
 function App() {
   return (
+    <GithubProvider>
   <Router>
     <div className='flex flex-col justify-between h-screen'>
       <NavBar/>
@@ -21,6 +23,7 @@ function App() {
       <Footer/>
     </div>
   </Router>
+  </GithubProvider>
   );
 }
 
